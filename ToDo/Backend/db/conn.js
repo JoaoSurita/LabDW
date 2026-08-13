5bff1dff9b5fffe8e9168418a3b48945182ae14f
JoaoSurita <joaopedrosurita490@gmail.com>
2026-08-13 09:31:14 -0300

    Arrumando conexão com o banco e Gerando Frontend

--- ToDo/Backend/db/conn.js
+++ ToDo/Backend/db/conn.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 // Duas formas de conexão, testar qual funciona na máquina
 async function main() {
     // await mongoose.connect('mongodb://localhost:27017/ToDo');
-    await mongoose.connect('mongodb://127.0.0.1.27017/ToDo');
+    await mongoose.connect('mongodb://127.0.0.1:27017/ToDo');
     console.log("Conectou MongoDb");
 }
 
